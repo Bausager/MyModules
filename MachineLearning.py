741edfbc87d0f57a1c1c9dc81c710161e2c59688
Michelle Bausager <Bausager.m@live.dk>
2019-04-22 20:37:55 +0200

    New

--- MachineLearning.py
+++ MachineLearning.py
@@ -4,20 +4,29 @@ import warnings
 import numpy as np
 from collections import Counter
 
-
 style.use('ggplot')
 
 
 # -----------------------------------------------------------------------------------------------------------------------------------------------
+def handle_non_numerical_data(df):
+	columns = df.columns.values
+	for column in columns:
+		text_digit_vals = {}
 
+		def convert_to_int(val):
+			return text_digit_vals[val]
 
-
-
-
-
-
+		if df[column].dtype != np.int64 and df[column].dtype != np.float64:
+			column_contents = df[column].values.tolist()
+			unique_elements = set(column_contents)
+			x = 0
+			for unique in unique_elements:
+				if unique not in text_digit_vals:
+					text_digit_vals[unique] = x
+					x += 1
+			df[column] = list(map(convert_to_int, df[column]))
+	return df
 # -----------------------------------------------------------------------------------------------------------------------------------------------
-
 
 
 class Nearest_Neighbors:
@@ -37,11 +46,7 @@ class Nearest_Neighbors:
 		vote_result = Counter(votes).most_common(1)[0][0]
 		confidence = Counter(votes).most_common(1)[0][1] / k
 		return vote_result, confidence
-
-
-
 # -----------------------------------------------------------------------------------------------------------------------------------------------
-
 
 
 class Support_Vector_Machine:
@@ -179,7 +184,52 @@ class Support_Vector_Machine:
 		self.ax.plot([hyp_x_min, hyp_x_max], [db1, db2], 'y--')
 
 		plt.show()
+# -----------------------------------------------------------------------------------------------------------------------------------------------
+
+
+class K_Means:
+	def __init__(self, k=2, tol=0.001, max_iter=300):
+		self.k = k
+		self.tol = tol
+		self.max_iter = max_iter
 
 
 
+	def fit(self, data):
+		self.centroids = {}
+
+		for i in range(self.k):
+			self.centroids[i] = data[i]
+
+		for i in range(self.max_iter):
+			self.classifications = {}
+
+			for i in range(self.k):
+				self.classifications[i] = []
+
+			for featureset in data:
+				distances = [np.linalg.norm(featureset - self.centroids[centroids]) for centroids in self.centroids]
+				classification = distances.index(min(distances))
+				self.classifications[classification].append(featureset)
+			prev_centroids = dict(self.centroids)
+
+			for classification in self.classifications:
+				self.centroids[classification] = np.average(self.classifications[classification], axis=0)
+
+			optimaized = True
+
+			for c in self.centroids:
+				original_centroids = prev_centroids[c]
+				current_centroids = self.centroids[c]
+				if np.sum((current_centroids - original_centroids) / original_centroids * 100.0) >= self.tol:
+					optimaized = False
+
+			if optimaized:
+				break
+
+
+	def predict(self, data):
+		distances = [np.linalg.norm(data - self.centroids[centroids]) for centroids in self.centroids]
+		classification = distances.index(min(distances))
+		return classification
 # -----------------------------------------------------------------------------------------------------------------------------------------------
