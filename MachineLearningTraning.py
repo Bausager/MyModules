e4849a94c86e73489c4862d776917ca84c9472c7
Michelle Bausager <Bausager.m@live.dk>
2019-04-23 16:19:45 +0200

    MachineLearning Done

--- MachineLearningTraning.py
+++ MachineLearningTraning.py
@@ -233,3 +233,93 @@ class K_Means:
 		classification = distances.index(min(distances))
 		return classification
 # -----------------------------------------------------------------------------------------------------------------------------------------------
+
+
+class Mean_Shift:
+	def __init__(self, radius=None, radius_norm_step=100):
+		self.radius = radius
+		self.radius_norm_step = radius_norm_step
+
+	def fit(self, data):
+
+		if self.radius is None:
+			all_data_centroid = np.average(data, axis=0)
+			all_data_norm = np.linalg.norm(all_data_centroid)
+			self.radius = all_data_norm / self.radius_norm_step
+
+		centroids = {}
+
+		for i in range(len(data)):
+			centroids[i] = data[i]
+
+		weights = [i for i in range(self.radius_norm_step)][::-1]
+
+		while True:
+			new_centroids = []
+			for i in centroids:
+				in_bandwitdh = []
+				centroid = centroids[i]
+				for featureset in data:
+					distances = np.linalg.norm(featureset - centroid)
+					if distances == 0:
+						distances = 0.00000000001
+					weight_index = int(distances / self.radius)
+					if weight_index > self.radius_norm_step - 1:
+						weight_index = self.radius_norm_step - 1
+					to_add = (weights[weight_index]**2) * [featureset]
+					in_bandwitdh += to_add
+
+				new_centroid = np.average(in_bandwitdh, axis=0)
+				new_centroids.append(tuple(new_centroid))
+
+			uniques = sorted(list(set(new_centroids)))
+
+			to_pop = []
+
+			for i in uniques:
+				for ii in uniques:
+					if i == ii:
+						pass
+					elif np.linalg.norm(np.array(i) - np.array(ii)) <= self.radius:
+						to_pop.append(ii)
+						break
+			for i in to_pop:
+				try:
+					uniques.remove(i)
+				except:
+					pass
+
+
+			prev_centroids = dict(centroids)
+			centroids = {}
+			for i in range((len(uniques))):
+				centroids[i] = np.array(uniques[i])
+			optimaized = True
+
+			for i in centroids:
+				if not np.array_equal(centroids[i], prev_centroids[i]):
+					optimaized = False
+				if not optimaized:
+					break
+			if optimaized:
+				break
+
+		self.centroids = centroids
+
+
+		self.classifications = {}
+
+		for i in range(len(self.centroids)):
+			self.classifications[i] = []
+		for featureset in data:
+			distances = [np.linalg.norm(featureset - self.centroids[centroid])for centroid in self.centroids]
+			classification = distances.index(min(distances))
+			self.classifications[classification].append(featureset)
+
+
+	def predict(self, data):
+		distances = [np.linalg.norm(featureset - self.centroids[centroid])for centroid in self.centroids]
+		classification = distances.index(min(distances))
+		return classification
+
+# -----------------------------------------------------------------------------------------------------------------------------------------------
